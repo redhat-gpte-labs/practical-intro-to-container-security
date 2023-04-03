7ebe0e68bbf4b50e029e0dca37facc68da6ca86a
Jason Kincl <jkincl@redhat.com>
2023-04-03 13:17:34 -0400

    Cleaning up old files that are not for our workshop

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,4 +1,5 @@
-WORKSHOP_NAME=ocp4-disconnected-install
+WORKSHOP_NAME=practical-intro-to-container-security
+WORKSHOP_TITLE="A Practical Introduction to Container Security"
 RESOURCE_BUDGET=medium
 MAX_SESSION_AGE=3600
 IDLE_TIMEOUT=300
